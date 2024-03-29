0a526044c8e77dd9fca468177ddf4a7e3ad5210e
qiaohongyuan 1111 <3511976110@qq.com>
2024-03-29 03:27:31 +0000

    123

--- src/main/java/Abstract/Shape.java
+++ src/main/java/Abstract/Shape.java
@@ -1,5 +1,7 @@
-package Abstract;
-
-public interface Shape {
-    void draw();
+public class Circle implements Shape {
+ 
+    @Override
+    public void draw() {
+       System.out.println("Inside Circle::draw() method.");
+    }
  }
